5f33d560a0d013d562ecc26ee0258f682189a3b7
vicanso <vicansocanbico@gmail.com>
2016-03-28 20:49:24 +0800

    use eslint-config-airbnb

--- test/performance.js
+++ test/performance.js
@@ -1,21 +1,22 @@
 'use strict';
 const assert = require('assert');
+const util = require('util');
 const performance = require('..');
 
 
 describe('performance-nodejs', () => {
-	it('get performance success', done => {
-		const timer = performance(data => {
-			assert(data.lag);
-			assert(data.heap.total_heap_size);
-			assert(data.heap.total_heap_size_executable);
-			assert(data.heap.total_physical_size);
-			assert(data.heap.total_available_size);
-			assert(data.heap.used_heap_size);
-			assert(data.heap.heap_size_limit);
-			timer.unref();
-			done();
-		});
-		timer.ref();
-	});
+  it('get performance success', done => {
+    const timer = performance(data => {
+      assert(util.isNumber(data.lag));
+      assert(data.heap.total_heap_size);
+      assert(data.heap.total_heap_size_executable);
+      assert(data.heap.total_physical_size);
+      assert(data.heap.total_available_size);
+      assert(data.heap.used_heap_size);
+      assert(data.heap.heap_size_limit);
+      timer.unref();
+      done();
+    });
+    timer.ref();
+  });
 });
